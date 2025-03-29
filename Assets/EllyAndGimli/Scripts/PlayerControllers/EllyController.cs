ba7ce12f95bcc379d4ca3c89de7b0c63fd437ca9
Shilov Vyacheslav <Sonic.x.exe@yandex.ru>
2025-03-29 16:59:22 +0500

    Добавил Элли скольжение по стенам

--- Assets/EllyAndGimli/Scripts/PlayerControllers/EllyController.cs
+++ Assets/EllyAndGimli/Scripts/PlayerControllers/EllyController.cs
@@ -1,26 +1,47 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class EllyController : PlayerControllerBase
 {
-	private bool _canMakeSecondJump = false;
+	private bool _canMakeSecondJump;
+	private bool _isTouchingWall;
+	private float _defaultGravityScale;
+
+	private readonly Func<Vector2, float, int, bool> _isOverlappingWithWall = (Vector2 position, float radius, int layerMask)
+		=> Physics2D.OverlapCircle(position, radius, layerMask) != null;
+
+	[Header("Связанное с коллизиями со стенами")]
+	[SerializeField] private Transform _leftWallCheck;
+	[SerializeField] private Transform _rightWallCheck;
+	[SerializeField] private float _wallCheckRadius = 0.2f;
+	[SerializeField] private LayerMask _wallLayer;
+	[SerializeField] private float _wallSlidingSpeedDown = 0.1f;
+	[SerializeField] private float _wallSlidingSpeedUp = 0.5f;
+
 
 	protected override void Start()
 	{
 		base.Start();
-		_inputSystemActions.Elly.Jump.started += context => TryDoJump(_isGrounded || _canMakeSecondJump);
+		_defaultGravityScale = _rb.gravityScale;
+		_inputSystemActions.Elly.Jump.started += context => TryDoJump((_isGrounded || _canMakeSecondJump) && !_isTouchingWall);
 	}
 
 	protected override void Update()
 	{
+		_isTouchingWall = _isOverlappingWithWall(_leftWallCheck.position, _wallCheckRadius, _wallLayer)
+			|| _isOverlappingWithWall(_rightWallCheck.position, _wallCheckRadius, _wallLayer);
+		_rb.gravityScale = _isTouchingWall ? _wallSlidingSpeedDown : _defaultGravityScale;
 		base.Update();
 	}
 
 	protected override void FixedUpdate()
 	{
 		var movement = _inputSystemActions.Elly.Move.ReadValue<Vector2>();
+		var isJumpPressed = _inputSystemActions.Elly.Jump.IsPressed();
 		DoMove(movement.x);
+		TryDoWallClimbing(isJumpPressed);
 		base.FixedUpdate();
 	}
 
@@ -32,6 +53,16 @@ public class EllyController : PlayerControllerBase
 			StartCoroutine(nameof(JumpCoroutine));
 		}
 		return hasJumped;
+	}
+
+	private bool TryDoWallClimbing(bool isJumpPressed)
+	{
+		if (isJumpPressed && _isTouchingWall)
+		{
+			_rb.linearVelocity = Vector2.up * _wallSlidingSpeedUp;
+			return true;
+		}
+		return false;
 	}
 
 	private IEnumerator JumpCoroutine()
